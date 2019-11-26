18a8c1a1ae55ce17c9f33fb47991c5cc24cf90ab
oudream <oudream@126.com>
2019-11-26 11:03:04 +0800

    first add

---
+++ readme.md
@@ -0,0 +1,17 @@
+# hello-chart
+
+
+https://fontawesome.com/icons
+
+
+### admin dashboard template
+
+https://github.com/ColorlibHQ/AdminLTE
+
+https://github.com/BlackrockDigital/startbootstrap-sb-admin-2
+
+https://github.com/creativetimofficial/light-bootstrap-dashboard
+
+https://github.com/kjdion84/bsadmin
+
+https://github.com/khansharukh/bootstrap-4-simple-sidebar-admin
---
+++ readme.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+
+open -a "/Applications/Google Chrome.app" 'http://localhost:8008/web/hello-chart/3rd/Highcharts-7/' && \
+open -a "/Applications/Google Chrome.app" 'http://localhost:8008/limi/gcl3-deploy-example/assets/gcl3/example/'
+
+http://localhost:8008/web/hello-chart/3rd/Highcharts-7/
+
+http://localhost:8008/web/hello-chart/projects/bsadmin/
+
+http://localhost:8008/web/hello-chart/projects/khansharukh/
+
+http://localhost:8008/limi/gcl3-deploy-example/assets/hello/fa/index.html
+
+
+### chart.js
+### awesome
+https://github.com/chartjs/awesome
+
+### doc
+https://www.chartjs.org/docs/latest/
+
+
+
+### highcharts
+
+### doc
+https://www.highcharts.com.cn/docs
+https://www.highcharts.com/docs
+
+
+### demo
+https://www.highcharts.com.cn/demo/highcharts
+
+
+### org download
+# https://www.highcharts.com/blog/download/
+wget https://code.highcharts.com/zips/Highcharts-7.2.1.zip
+wget https://code.highcharts.com/zips/Highstock-7.2.1.zip
+wget https://code.highcharts.com/zips/Highmaps-7.2.1.zip
+wget https://code.highcharts.com/zips/Highcharts-Gantt-7.2.1.zip
+
+### source code
+git clone https://github.com/highcharts/highcharts
+
+
