1256941829355a39c71cbd384750649fd99c5fd1
oudream <oudream@126.com>
2020-02-07 12:54:40 +0800

    Update 0.readme.sh

--- assets/hello/css/0.readme.sh
+++ assets/hello/css/0.readme.sh
@@ -2,3 +2,55 @@
 
 # css secrets
 open "http://play.csssecrets.io/"
+
+
+open https://www.w3.org/TR/CSS2/
+open https://zh.wikipedia.org/wiki/%E5%B1%82%E5%8F%A0%E6%A0%B7%E5%BC%8F%E8%A1%A8
+open http://www.runoob.com/css/css-examples.html
+open http://www.runoob.com/css/css-tutorial.html
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/Reference
+open https://developer.mozilla.org/zh-CN/docs/Web/css
+open https://developer.mozilla.org/zh-CN/docs/Learn/CSS/Introduction_to_CSS/Selectors
+open http://design.1sters.com/
+open https://material.io/guidelines/material-design/introduction.html#
+open https://design.google.com/
+open https://www.google.com.hk/search?q=Material+Design&oq=Material+Design&aqs=chrome..69i57.409j0j4&sourceid=chrome&ie=UTF-8
+open http://www.w3school.com.cn/cssref/pr_class_display.asp
+open https://www.w3.org/TR/CSS21/propidx.html
+open https://segmentfault.com/q/1010000000269211
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/Replaced_element
+open https://www.w3schools.com/jsref/dom_obj_style.asp
+open https://facebook.github.io/react/docs/react-component.html
+open https://www.youtube.com/playlist?list=PLmOn9nNkQxJHcQxkxxZ36S9t_86QNgWvG
+open https://www.youtube.com/playlist?list=PL5_bXwQAVebWSIka4OH197eWUyqnB-TR0
+open https://www.youtube.com/watch?v=-tZNci6Ajos
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/At-rule
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/Using_CSS_custom_properties
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/transform
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/Value_definition_syntax
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/CSS_Types
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/color_value#rgb()
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/filter
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/CSS_Box_Model
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/All_About_The_Containing_Block
+open https://developer.mozilla.org/zh-CN/docs/Web/Guide/CSS/Understanding_z_index
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/Layout_cookbook
+open https://developer.mozilla.org/zh-CN/docs/Web/CSS/used_value
+open http://gdut_yy.gitee.io/doc-csstdg4/#%E7%9B%AE%E5%BD%95
+open https://github.com/gdut-yy/CSS-The-Definitive-Guide-4th-zh
+open http://gdut_yy.gitee.io/doc-csstdg4/
+open https://webdesign.tutsplus.com/categories/css
+
+
+open https://github.com/LeaVerou?tab=repositories
+open https://codepen.io/leaverou/
+open http://play.csssecrets.io/
+open https://github.com/LeaVerou/css3test
+open https://css3test.com/
+open https://www.samanthaming.com/
+open https://www.samanthaming.com/flexbox30/
+open https://github.com/samanthaming?tab=repositories
+open https://github.com/samanthaming/Flexbox30
+open https://github.com/sawmac/css_mm_4e
+open https://www.jianshu.com/p/74c451666d4b
